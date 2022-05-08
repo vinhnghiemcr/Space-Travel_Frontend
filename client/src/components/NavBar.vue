5c413b6835b31ddc9d6f562920c2b3056d64b4bb
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-05-08 00:27:49 -0500

    Created pages & components folder

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <router-link to="/" name="HomePage">Home</router-link>
+    <router-link to="/listings">Listings</router-link>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar'
+  }
+</script>
---
+++ client/src/pages/AboutPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Space-Booking Website</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AboutPage'
+}
+</script>
